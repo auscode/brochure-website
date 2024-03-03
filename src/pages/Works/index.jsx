c484964813c04fd468dfb79547ff12eae22065cc
auscode <harshittext@gmail.com>
2024-03-03 13:51:39 +0530

    WORK resopnive sm

--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -11,13 +11,12 @@ const WorksPage = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleMenuToggle = () => {
-    // console.log("handleMenuToggle");
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <>
-      <div className="bg-white-A700 flex flex-col font-sourcesanspro gap-[54px] sm:gap-5 items-start justify-start mx-auto w-full">
+      <div className="bg-white-A700 flex flex-col font-sourcesanspro gap-[54px] sm:gap-5 items-start justify-start mx-auto w-full overflow-hidden">
         {/* <div className="flex flex-col items-center w-full"> */}
         <header className="flex items-center justify-center md:px-5 sm:px-0 w-full">
           <div className="flex flex-row md:gap-10 items-center justify-between ml-[164px] mr-[181px] my-[15px] w-[76%] sm:ml-0 sm:mr-0 sm:my-1 sm:w-[90%] ">
@@ -116,7 +115,7 @@ const WorksPage = () => {
         </div>
         {/* </div> */}
         <div className="flex flex-col items-start justify-start w-[92%] md:px-5 sm:px-0 md:w-full">
-          <div className="bg-green-A100 sm:h-[580px] h-[646px] md:h-[930px] md:ml-[0] ml-auto p-1 relative rounded-[90px] w-[86%] md:w-full">
+          <div className="bg-green-A100 sm:h-[400px] h-[646px] md:h-[930px] md:ml-[0] ml-auto p-1 relative rounded-[90px] w-[86%] md:w-full">
             <div className="absolute top-[25%] flex flex-col md:gap-10 gap-[82px] h-full justify-start m-auto">
               <Text
                 className="text-center sm:text-[40px] md:text-[46px] text-[50px] text-black-900"
@@ -133,7 +132,7 @@ const WorksPage = () => {
                 </span>
               </Text>
               <Text
-                className="leading-[40.00px] text-center text-gray-700 text-2xl w-full px-32"
+                className="leading-[40.00px] text-center text-gray-700 text-2xl sm:text-xl w-full px-32 sm:px-0"
                 size="txtSourceSansProRegular20Gray700"
               >
                 We have completed many projects from various companies in the
@@ -141,9 +140,9 @@ const WorksPage = () => {
                 with the results we have provided
               </Text>
             </div>
-            <div className="absolute sm:static bottom-[1%] flex flex-col inset-x-[0] items-center justify-start mx-auto w-[96%]">
+            <div className="absolute sm:hidden bottom-[1%] flex flex-col inset-x-[0] items-center justify-start mx-auto w-[96%]">
               <div className="flex flex-col justify-start w-full">
-                <div className="flex flex-row sm:gap-10 gap-[767px] items-start justify-start w-4/5 md:w-full">
+                <div className="flex flex-row sm:gap-10 gap-[767px]  items-start justify-start w-4/5 md:w-full">
                   <Img
                     className="h-[114px] rounded-[10px] w-[114px]"
                     src="images/img_rectangle57.svg"
@@ -169,7 +168,7 @@ const WorksPage = () => {
             </div>
           </div>
           <div className="flex flex-col gap-10 items-start justify-start md:ml-[0] ml-[129px] mt-[100px] pt-[17px] w-[91%] md:w-full">
-            <div className="flex md:flex-col flex-row gap-[58px] items-center justify-start w-[59%] md:w-full">
+            <div className="flex md:flex-col flex-row gap-[58px] items-center sm:items-start sm:px-1 justify-start w-[59%] md:w-full">
               <Text
                 className="sm:text-4xl md:text-[38px] text-[40px] text-black-900"
                 size="txtSourceSansProBold40"
@@ -185,14 +184,14 @@ const WorksPage = () => {
             </div>
             <div className="flex flex-col items-start justify-start w-full">
               <div className="md:h-[496px] h-[527px] relative w-[95%] md:w-full ml-auto">
-                <div className="absolute bg-orange-A200 h-[505px] inset-x-[0] mx-auto rounded-[30px] top-[0] w-full"></div>
-                <div className="absolute bottom-[0] h-[482px] right-[0] w-[92%] md:w-full">
+                <div className="absolute bg-orange-A200 h-[505px] sm:h-[360px] inset-x-[0] mx-auto rounded-[30px] top-[0] w-full"></div>
+                <div className="absolute bottom-[0] sm: h-[482px] right-[0] w-[92%] md:w-full">
                   <Img
-                    className="absolute h-[482px] inset-y-[0] my-auto object-cover right-[0] w-[73%]"
+                    className="absolute sm:static h-[482px] inset-y-[0] my-auto object-cover sm:object-contain right-[0] w-[73%] sm:w-full"
                     src="images/img_group21.png"
                     alt="groupTwentyOne"
                   />
-                  <div className="absolute flex flex-col items-start justify-start left-[0] top-[0] w-[28%]">
+                  <div className="absolute flex flex-col sm:m-4 items-start justify-start left-[0] top-[0] w-[28%] sm:w-full">
                     <Text
                       className="sm:text-[31px] md:text-[33px] text-[35px] text-white-A700"
                       size="txtNunitoExtraBold35"
@@ -200,48 +199,48 @@ const WorksPage = () => {
                       Foodie.
                     </Text>
                     <Text
-                      className="mt-[136px] sm:text-[31px] md:text-[33px] text-[35px] text-white-A700 w-full"
+                      className="mt-[136px] sm:mt-2 sm:text-[20px] md:text-[33px] text-[35px] text-white-A700 w-full"
                       size="txtSourceSansProBold35"
                     >
                       Launching website for Apps
                     </Text>
-                    <Line className="bg-white-A700 h-0.5 mt-[68px] w-[24%]" />
+                    <Line className="bg-white-A700 h-0.5 mt-[68px] sm:mt-2 w-[24%]" />
                   </div>
                 </div>
               </div>
               {/* <div className="md:h-[1034px] h-[580px] sm:h-[680px] mt-[100px] w-[95%] relative md:w-full ml-auto">
                 <div className="absolute bg-indigo-800 h-[580px] inset-y-[0] left-[0] my-auto rounded-[30px] w-full"></div> */}
-              <div className="md:h-[1034px] h-[580px] sm:h-[680px] mt-[100px] w-[95%] relative md:w-full ml-auto">
-                <div className="absolute bg-indigo-800 h-[580px] inset-y-[0] left-[0] my-auto rounded-[30px] w-full">
-                  <div className="absolute flex md:flex-col flex-row md:gap-10 items-end justify-between right-[0] top-[7%] w-[92%]">
-                    <div className="flex flex-col items-start justify-start md:mt-0 mt-5">
+              <div className="md:h-[1034px] h-[580px] sm:h-[600px] mt-[100px] sm:mt-0 w-[95%] relative md:w-full ml-auto">
+                <div className="absolute bg-indigo-800 h-[580px] sm:h-[500px] inset-y-[0] left-[0] my-auto rounded-[30px] w-full">
+                  <div className="absolute sm:static flex md:flex-col flex-row md:gap-10 items-end sm:items-start justify-between right-[0] top-[7%] sm:w-full w-[92%]">
+                    <div className="flex flex-col items-start justify-start md:mt-0 mt-5 sm:m-6 ">
                       <Text
-                        className="bg-clip-text bg-gradient  sm:text-[31px] md:text-[33px] text-[35px] text-transparent"
+                        className="bg-clip-text bg-gradient sm:text-[31px] md:text-[33px] text-[35px] text-transparent"
                         size="txtSourceSansProBold35OrangeA200"
                       >
                         Floops
                       </Text>
                       <Text
-                        className="mt-[193px] sm:text-[31px] md:text-[33px] text-[35px] text-white-A700 w-full"
+                        className="mt-[193px] sm:mt-2 sm:text-[20px] md:text-[33px] text-[35px] text-white-A700 w-full"
                         size="txtSourceSansProBold35"
                       >
                         Landing page for Website Service
                       </Text>
-                      <Line className="bg-white-A700 h-0.5 mt-[68px] w-[24%]" />
+                      <Line className="bg-white-A700 h-0.5 mt-[68px] sm:mt-2 w-[24%]" />
                     </div>
                     <Img
-                      className="absolute left-[50%] top-[10%] h-[456px] md:h-auto "
+                      className="absolute sm:static left-[50%] top-[10%] h-[456px] md:h-auto "
                       src="images/img_group790.png"
                       alt="group790"
                     />
                   </div>
                 </div>
               </div>
-              <div className="bg-red-A200 h-[580px] md:h-[690px] mt-[110px] relative ml-auto rounded-[30px] w-[95%] md:w-full">
-                <div className="flex flex-col h-full items-start justify-start ml-[101px] my-auto w-[26%] pt-28">
-                  <div className="flex flex-row gap-4 items-start justify-start w-[58%] md:w-full">
+              <div className="bg-red-A200 h-[580px] md:h-[690px] sm:h-[579px] mt-[110px] sm:mt-0 relative ml-auto rounded-[30px] w-[95%] md:w-full">
+                <div className="flex flex-col h-full items-start justify-start ml-[101px] sm:ml-4 my-auto w-[26%] sm:w-[60%] pt-28 sm:pt-12">
+                  <div className="flex flex-row gap-4 items-start justify-start w-[58%] sm:w-full md:w-full">
                     <Img
-                      className="h-[38px] md:h-auto object-cover w-[31%]"
+                      className="h-[38px] md:h-auto object-cover w-[31%] sm:w-[25%]"
                       src="images/img_group511.png"
                       alt="Group511"
                     />
@@ -252,22 +251,22 @@ const WorksPage = () => {
                     />
                   </div>
                   <Text
-                    className="mt-[173px] sm:text-[31px] md:text-[33px] text-[35px] text-white-A700 w-full"
+                    className="mt-[173px] sm:mt-4 sm:text-[20px] md:text-[33px] text-[35px] text-white-A700 w-full sm:w-full"
                     size="txtSourceSansProBold35"
                   >
                     Website for creative agency
                   </Text>
-                  <Line className="bg-white-A700 h-0.5 mt-[68px] w-[24%]" />
+                  <Line className="bg-white-A700 h-0.5 mt-[68px] sm:mt-4 w-[24%]" />
                 </div>
                 <div className="absolute flex flex-col h-full inset-[0] items-center justify-center m-auto w-full">
                   <div className="h-[580px] relative w-full">
                     <Img
-                      className="h-[580px] ml-auto my-auto object-cover rounded-[30px] w-[32%]"
+                      className="h-[580px] sm:hidden ml-auto my-auto object-cover sm:object-fill rounded-[30px] w-[32%]"
                       src="images/img_web192021.png"
                       alt="Web192021"
                     />
                     <Img
-                      className="absolute h-[580px] inset-[0] justify-center m-auto object-cover rounded-[30px] w-full"
+                      className="absolute sm:static h-[580px] inset-[0] justify-center m-auto object-cover rounded-[30px] w-full"
                       src="images/img_web192021_580X1181.png"
                       alt="Web192021 One"
                     />
